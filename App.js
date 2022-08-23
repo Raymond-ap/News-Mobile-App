6dc2c6fd80bd27202c1d3b65f25e74a7c24685ee
Apungu Raymond <apunguray@gmail.com>
2022-08-24 04:48:17 +0800

    local update

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { TailwindProvider } from "tailwindcss-react-native";
 import { HomeScreen, NewsDetail } from "./screens";
+import { AuthProvider } from "./hooks/useAuth";
 
 const Stack = createNativeStackNavigator();
 
@@ -11,13 +12,15 @@ export default function App() {
   return (
     <TailwindProvider>
       <NavigationContainer>
-        <Stack.Navigator
-          initialRouteName="Home"
-          screenOptions={{ headerShown: false }}
-        >
-          <Stack.Screen name="Home" component={HomeScreen} />
-          <Stack.Screen name="NewsDetail" component={NewsDetail} />
-        </Stack.Navigator>
+        <AuthProvider>
+          <Stack.Navigator
+            initialRouteName="Home"
+            screenOptions={{ headerShown: false }}
+          >
+            <Stack.Screen name="Home" component={HomeScreen} />
+            <Stack.Screen name="NewsDetail" component={NewsDetail} />
+          </Stack.Navigator>
+        </AuthProvider>
       </NavigationContainer>
       <StatusBar style="auto" translucent={false} backgroundColor={"#fff"} />
     </TailwindProvider>
